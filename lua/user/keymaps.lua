e50c226439f0dcd30b34c776ae24788ef8e5860e
dddddjent <1306043330a@gmail.com>
2022-12-29 13:05:18 +0800

    Remove session

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -112,3 +112,4 @@ keymap("n", "<C-t>", "<Cmd>Telescope<CR>", opts)
 -- Replace
 vim.keymap.set("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
