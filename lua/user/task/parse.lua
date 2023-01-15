7f8bd4b7b771dd1d9e514f156757d661bb6f3d7d
dddddjent <1306043330a@gmail.com>
2023-01-15 11:58:38 +0800

    json5 for tasks

--- lua/user/task/parse.lua
+++ lua/user/task/parse.lua
@@ -3,7 +3,7 @@ local M = {}
 M.get_object = function()
     io.input(vim.fn.getcwd() .. "/.vscode/tasks.json")
     local str = io.read("*a")
-    local json_obj = vim.json.decode(str)
+    local json_obj = require 'json5'.parse(str)
 
     return json_obj
 end
