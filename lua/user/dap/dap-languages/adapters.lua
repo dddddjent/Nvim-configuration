dc86f84c0c5efb5af569b11f6b5f2725427bf0b0
dddddjent <1306043330a@gmail.com>
2023-07-27 14:04:18 +0800

    Python attach?

--- lua/user/dap/dap-languages/adapters.lua
+++ lua/user/dap/dap-languages/adapters.lua
@@ -1,9 +1,29 @@
 local dap = require('dap')
-dap.adapters.python = {
-    type = 'executable';
-    command = 'python';
-    args = { '-m', 'debugpy.adapter' };
-}
+dap.adapters.python = function(cb, config)
+    if config.request == 'attach' then
+        ---@diagnostic disable-next-line: undefined-field
+        local port = (config.connect or config).port
+        ---@diagnostic disable-next-line: undefined-field
+        local host = (config.connect or config).host or '127.0.0.1'
+        cb({
+            type = 'server',
+            port = assert(port, '`connect.port` is required for a python `attach` configuration'),
+            host = host,
+            options = {
+                source_filetype = 'python',
+            },
+        })
+    else
+        cb({
+            type = 'executable',
+            command = '/usr/bin/python',
+            args = { '-m', 'debugpy.adapter' },
+            options = {
+                source_filetype = 'python',
+            },
+        })
+    end
+end
 dap.adapters.cppdbg = {
     id = 'cppdbg',
     type = 'executable',
@@ -17,7 +37,7 @@ dap.adapters.codelldb = {
         -- command = '/usr/bin/codelldb',
         command = (function()
             if OS == "windows" then return 'codelldb.cmd'
-            -- else return '/usr/bin/codelldb'
+                -- else return '/usr/bin/codelldb'
             else return '/home/ljl/.local/share/nvim/mason/bin/codelldb'
             end
         end)(),
