4d2c557f1a9063b55496e46e899f65478e6002bd
dddddjent <1306043330a@gmail.com>
2023-09-11 00:01:28 +0800

    Use better-escape to escape

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -45,8 +45,8 @@ keymap("n", "z.", "zszH", opts)
 
 -- Insert --
 -- Press jk fast to exit insert mode
-keymap("i", "jk", "<ESC>", opts)
-keymap("i", "kj", "<ESC>", opts)
+-- keymap("i", "jk", "<ESC>", opts)
+-- keymap("i", "kj", "<ESC>", opts)
 
 -- Visual --
 -- Stay in indent mode
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -335,5 +335,23 @@ require("lazy").setup({
                 },
             }
         end
+    },
+
+    -- lua with packer.nvim
+    {
+        "max397574/better-escape.nvim",
+        config = function()
+            -- lua, default settings
+            require("better_escape").setup {
+                mapping = { "kj","jk"  }, -- a table with mappings to use
+                timeout = 100, -- the time in which the keys must be hit in ms. Use option timeoutlen by default
+                clear_empty_lines = false, -- clear line after escaping if there is only whitespace
+                keys = "<Esc>", -- keys used for escaping, if it is a function will use the result everytime
+                -- example(recommended)
+                -- keys = function()
+                --   return vim.api.nvim_win_get_cursor(0)[2] > 1 and '<esc>l' or '<esc>'
+                -- end,
+            }
+        end,
     }
 })
