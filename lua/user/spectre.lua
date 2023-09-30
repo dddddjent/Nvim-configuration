c73481059bce0bf2f78f23c0134ae35cc6078d05
dddddjent <1306043330a@gmail.com>
2023-10-01 00:28:10 +0800

    Compile oxi automatically for spectre

--- lua/user/spectre.lua
+++ lua/user/spectre.lua
@@ -3,6 +3,24 @@ if OS == 'windows' then
     rg_cmd = vim.fn.stdpath('config') .. '/bin/rg.exe'
 end
 
+local function file_exists(path)
+    local file = io.open(path, "rb")
+    if file then file:close() end
+    return file ~= nil
+end
+
+local spectre_path = vim.fn.stdpath("data") .. "/lazy/nvim-spectre/"
+if file_exists(spectre_path .. 'lua/spectre_oxi.so') == false then
+    print("Compiling nvim-oxi for nvim-spectre")
+
+    local cmd = "cd " .. spectre_path .. ';./build.sh nvim-oxi'
+    os.execute(cmd)
+
+    print("Compilation done")
+    print()
+    print('PLEASE restart to use oxi')
+end
+
 require('spectre').setup({
     find_engine = {
         -- rg is map with finder_cmd
