0d334afaefbafef293db1023e9113ca6da0f9fd4
dddddjent <1306043330a@gmail.com>
2023-10-14 23:08:30 +0800

    No need to compile nvim-oxi, just use gnu-sed for Windows

--- lua/user/spectre.lua
+++ lua/user/spectre.lua
@@ -3,23 +3,23 @@ if OS == 'windows' then
     rg_cmd = vim.fn.stdpath('config') .. '/bin/rg.exe'
 end
 
-local function file_exists(path)
-    local file = io.open(path, "rb")
-    if file then file:close() end
-    return file ~= nil
-end
+-- local function file_exists(path)
+--     local file = io.open(path, "rb")
+--     if file then file:close() end
+--     return file ~= nil
+-- end
 
-local spectre_path = vim.fn.stdpath("data") .. "/lazy/nvim-spectre/"
-if file_exists(spectre_path .. 'lua/spectre_oxi.so') == false then
-    print("Compiling nvim-oxi for nvim-spectre")
-
-    local cmd = "cd " .. spectre_path .. ';./build.sh nvim-oxi'
-    os.execute(cmd)
-
-    print("Compilation done")
-    print()
-    print('PLEASE restart to use oxi')
-end
+-- local spectre_path = vim.fn.stdpath("data") .. "/lazy/nvim-spectre/"
+-- if file_exists(spectre_path .. 'lua/spectre_oxi.so') == false then
+--     print("Compiling nvim-oxi for nvim-spectre")
+--
+--     local cmd = "cd " .. spectre_path .. ';./build.sh nvim-oxi'
+--     os.execute(cmd)
+--
+--     print("Compilation done")
+--     print()
+--     print('PLEASE restart to use oxi')
+-- end
 
 require('spectre').setup({
     find_engine = {
@@ -44,9 +44,9 @@ require('spectre').setup({
             "ignore-case"
         }
     },
-    default = {
-        replace = {
-            cmd = "oxi"
-        }
-    }
+    -- default = {
+    --     replace = {
+    --         cmd = "oxi"
+    --     }
+    -- }
 })
