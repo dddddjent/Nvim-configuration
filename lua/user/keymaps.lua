3db26e1a30ebe6fe6082c6b7321da012923e0aaf
dddddjent <1306043330a@gmail.com>
2023-10-26 18:05:50 +0800

    No fucking J to move text in Visual Block mode

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -60,8 +60,8 @@ keymap("v", "p", '"_dP', opts)
 
 -- Visual Block --
 -- Move text up and down
-keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
+-- keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
+-- keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
 keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 
