6856b5087307a9b0c07c4dd8177ee12bda0b4e43
dddddjent <1306043330a@gmail.com>
2023-12-04 15:17:09 +0800

    Nvim tree keymaps recovered

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -11,6 +11,12 @@ local function on_attach(bufnr)
     local function opts(desc)
         return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
     end
+    vim.keymap.set('n', 'p', api.fs.paste, opts('Paste'))
+    vim.keymap.set('n', 'y', api.fs.copy.filename, opts('Copy Name'))
+    vim.keymap.set('n', 'x', api.fs.cut, opts('Cut'))
+    vim.keymap.set('n', 'c', api.fs.copy.node, opts('Copy'))
+    vim.keymap.set('n', 'd', api.fs.remove, opts('Delete'))
+    vim.keymap.set('n', 'a', api.fs.create, opts('Create'))
     vim.keymap.set('n', 'q', api.tree.close, opts('Close'))
     vim.keymap.set('n', 'l', api.node.open.edit, opts('Open'))
     vim.keymap.set('n', '<CR>', api.node.open.edit, opts('Open'))
