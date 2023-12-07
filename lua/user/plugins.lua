70dc27ec3cfbcedd7d569292cdf2d95ab3d80d24
dddddjent <1306043330a@gmail.com>
2023-12-07 11:23:32 +0800

    fzf native should not rely on cmake. make is enough

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -161,8 +161,7 @@ require("lazy").setup({
     },
     {
         'nvim-telescope/telescope-fzf-native.nvim',
-        build =
-        'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
+        build = 'make',
     },
     {
         'nvim-pack/nvim-spectre',
