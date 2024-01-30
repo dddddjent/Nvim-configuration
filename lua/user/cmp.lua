6c99c480f48b874a344a5fb1776f77925ded6a14
dddddjent <1306043330a@gmail.com>
2024-01-30 20:33:28 +0800

    cmp-cmdline

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -154,4 +154,25 @@ cmp.setup {
         },
     },
 }
+-- `/` cmdline setup.
+cmp.setup.cmdline('/', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+        { name = 'buffer' }
+    }
+})
+-- `:` cmdline setup.
+cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+        { name = 'path' }
+    }, {
+        {
+            name = 'cmdline',
+            option = {
+                ignore_cmds = { 'Man', '!' }
+            }
+        }
+    })
+})
 -- require("html-css"):setup()
