c96bb980cd5712f17b26c1cc91926bd3f6b14035
dddddjent <1306043330a@gmail.com>
2024-06-15 22:34:17 +0800

    Fix alpha find file icon

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -13,7 +13,7 @@ dashboard.section.header.val = {
     [[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
 }
 dashboard.section.buttons.val = {
-    dashboard.button("f", "  Find file", ":FzfLua files <CR>"),
+    dashboard.button("f", "󰈞  Find file", ":FzfLua files <CR>"),
     dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
     dashboard.button("r", "  Recently used files", ":FzfLua oldfiles <CR>"),
     dashboard.button("t", "󱎸  Find text", ":FzfLua live_grep <CR>"),
