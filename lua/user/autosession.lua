53c3bc3b7eaf35d9e7e7a0644e878a0d61d0ae26
dddddjent <1306043330a@gmail.com>
2024-10-07 16:34:53 -0400

    update gitsigns/ufo, fix autosessions

--- lua/user/autosession.lua
+++ lua/user/autosession.lua
@@ -1,11 +1,11 @@
 require('auto-session').setup(
     {
         log_level = 'error',
-        auto_session_enable_last_session = true,
+        -- auto_session_enable_last_session = true,
         auto_session_root_dir = vim.fn.stdpath('data') .. "/.sessions/",
         auto_session_enabled = true,
         auto_save_enabled = true,
-        auto_restore_enabled = false,
+        auto_restore_enabled = true,
         auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
         auto_session_use_git_branch = nil,
         -- the configs below are lua only
