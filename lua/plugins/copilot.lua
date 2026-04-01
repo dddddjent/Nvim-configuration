95e1acdf8fbee512e9cda4be85385cf74b9eb467
dddddjent <1306043330a@gmail.com>
2026-04-01 18:10:23 -0400

    Setup copilot correctly

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,3 +1,30 @@
 return {
-    "github/copilot.vim"
+    "github/copilot.vim",
+    lazy = false,
+    init = function()
+        vim.g.copilot_node_command = MISE_DEP_DIR .. "/node/latest/bin/node"
+        vim.g.copilot_no_tab_map = true
+    end,
+    keys = {
+        {
+            "<C-f>",
+            'copilot#Accept("\\<CR>")',
+            desc = "Copilot Accept",
+            expr = true,
+            replace_keycodes = false,
+            mode = "i",
+        },
+        {
+            "<C-n>",
+            "<Plug>(copilot-next)",
+            desc = "Copilot Next",
+            mode = "i",
+        },
+        {
+            "<C-x>",
+            "<Plug>(copilot-dismiss)",
+            desc = "Copilot dismiss",
+            mode = "i",
+        },
+    }
 }
