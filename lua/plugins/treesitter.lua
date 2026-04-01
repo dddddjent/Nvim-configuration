d1c7526adfe1807efb0da2f8f1de2f4190630ea8
dddddjent <1306043330a@gmail.com>
2026-04-01 10:56:29 -0400

    treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,13 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    build = ":TSUpdate",
+    config = function()
+        require("nvim-treesitter").install({
+            "lua", "vim", "html", "javascript", "css",
+            "cpp", "java", "rust", "python", "typescript",
+            'json', "cmake", "latex", "verilog", "bash", "tsx", "go", "gomod",
+            "toml", "yaml", "xml"
+        })
+    end,
+}
